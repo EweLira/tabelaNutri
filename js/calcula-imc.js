fbe907531ef10534f22f31e180c924160e4ed7d2
Ewellyn Lira <ewellynflira@gmail.com>
2022-11-09 14:32:28 -0300

    validaçãoform

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -15,26 +15,50 @@ for(var i = 0; i < pacientes.length; i++){
 
     var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoValido = true;
-    var alturaValida = true;
+    var pesoValido = validaPeso(peso);
+    var alturaValida = validaAltura(altura);
 
-    if (peso <= 0 || peso >= 1000){
+    if (!pesoValido){
         console.log("Peso inválido!");
         pesoValido = false;
         tdImc.textContent = "Peso Inválido!";
         paciente.classList.add("paciente-invalido")
     }
-    if (altura <= 0 || altura >= 3.00){
+    if (!alturaValida){
         console.log("Altura inválida!");
         alturaValida = false;
         tdImc.textContent = "Altura Inválida!"
         paciente.classList.add("paciente-invalido")
     }
-    if(alturaValida && pesoValido){
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+    if(pesoValido && alturaValida){
+        var imc = calculaImc(peso, altura);    
+        tdImc.textContent = imc;
     }
 }
 
+function validaPeso(peso){
+    if(peso >= 0  && peso < 1000){
+        return true;
+    }else{
+        return false;
+    }
+}
+function validaAltura(altura){
+    if(altura >= 0  && altura <= 3.00){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function calculaImc(peso, altura) {
+        var imc = 0;
+        imc = peso / (altura * altura);
+        return imc.toFixed(2)
+}
+
+
+
+    
 
 
