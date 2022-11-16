317686733f8c4a5203a444ad3774f9f30054260a
Ewellyn Lira <ewellynflira@gmail.com>
2022-11-16 19:47:29 -0300

    remocao/Filtro/Ajax

--- js/form.js
+++ js/form.js
@@ -5,8 +5,6 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
         var form = document.querySelector("#form-adiciona");
         var paciente = obtemPacienteDoFormulario(form);
 
-        var pacienteTr = montaTr(paciente);
-
         var erros = validaPaciente(paciente);
       
 
@@ -17,16 +15,26 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
             return;
         }
 
-
-        var tabela = document.querySelector("#tabela-pacientes");
-
-        tabela.appendChild(pacienteTr);
-
+        
+        adicionaPacienteNaTabela(paciente);
+        
         form.reset();
         var mensagensErro = document.querySelector("#mensagens-erro");
         mensagensErro.innerHTML= "";
 
 });
+
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
+
+
+
+
+
 function exibeMensagemDeErro(erros){
     var ul = document.querySelector("#mensagens-erro");
     ul.innerHTML = "";
